354d8d6c5e5cc1a9972d963339fb108e55733fb7
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:01:14 +0530

    feat(dsa): Solved Check if LL is palindrome or not [Easy]

---
+++ Linked List/check if ll is palindrome or not/solution.java
@@ -0,0 +1,10 @@
+// Problem: Check if LL is palindrome or not (Easy)
+// Topic: Linked List
+// Link: https://leetcode.com/problems/palindrome-linked-list/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
