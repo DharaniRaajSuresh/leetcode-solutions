d5d39af6156887008a3cda5ab40bb84f745632f2
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:00:47 +0530

    feat(dsa): Solved Remove Nth node from the back of the LL [Medium]

---
+++ Linked List/remove nth node from the back of the ll/solution.java
@@ -0,0 +1,10 @@
+// Problem: Remove Nth node from the back of the LL (Medium)
+// Topic: Linked List
+// Link: https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
