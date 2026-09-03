4e97727cec4b23481d0595b7a40acfd18c1b0aa5
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 16:59:15 +0530

    feat(dsa): Solved Find the repeating and missing number [Easy]

---
+++ Arrays/find the repeating and missing number/solution.java
@@ -0,0 +1,10 @@
+// Problem: Find the repeating and missing number (Easy)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/set-mismatch/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
