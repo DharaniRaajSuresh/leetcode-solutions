7548a39f92b6998c63af93ca94f96e7d70b769ea
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:01:23 +0530

    feat(dsa): Solved Flattening of LL [Medium]

---
+++ Linked List/flattening of ll/solution.java
@@ -0,0 +1,10 @@
+// Problem: Flattening of LL (Medium)
+// Topic: Linked List
+// Link: https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
