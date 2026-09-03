c291978861937aed975e04551a6330b4d3e35ca7
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:03:04 +0530

    feat(dsa): Solved Longest Common Prefix [Easy]

---
+++ String Algorithms/longest common prefix/solution.java
@@ -0,0 +1,10 @@
+// Problem: Longest Common Prefix (Easy)
+// Topic: String Algorithms
+// Link: https://leetcode.com/problems/longest-common-prefix/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
