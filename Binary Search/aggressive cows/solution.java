3f3b9f58b33fc602a5c4eaf2c8309e2c748f42d8
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:02:42 +0530

    feat(dsa): Solved Aggressive Cows [Medium]

---
+++ Binary Search/aggressive cows/solution.java
@@ -0,0 +1,10 @@
+// Problem: Aggressive Cows (Medium)
+// Topic: Binary Search
+// Link: https://leetcode.com/problems/magnetic-force-between-two-balls/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
