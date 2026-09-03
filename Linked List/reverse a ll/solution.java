41935a7832b618bb1b2153d99132a38506566537
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:00:32 +0530

    feat(dsa): Solved Reverse a LL [Easy]

---
+++ Linked List/reverse a ll/solution.java
@@ -0,0 +1,10 @@
+// Problem: Reverse a LL (Easy)
+// Topic: Linked List
+// Link: https://leetcode.com/problems/reverse-linked-list/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
