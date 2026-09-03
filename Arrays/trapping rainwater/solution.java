598887d5e2566812fe76338ad34ce0e87574b98c
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:01:37 +0530

    feat(dsa): Solved Trapping Rainwater [Hard]

---
+++ Arrays/trapping rainwater/solution.java
@@ -0,0 +1,10 @@
+// Problem: Trapping Rainwater (Hard)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/trapping-rain-water/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
