5a4dec9624251100016f790566fecdb4fc8a4dee
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 21:27:57 +0530

    feat(dsa): Solved Print root to leaf path in BT [Easy]

---
+++ Binary Trees/print root to leaf path in bt/solution.java
@@ -0,0 +1,10 @@
+// Problem: Print root to leaf path in BT (Easy)
+// Topic: Binary Trees
+// Link: https://leetcode.com/problems/binary-tree-paths/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
