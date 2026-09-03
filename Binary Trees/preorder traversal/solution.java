0d215367e639d28cbe457a00329965fc3a0a087f
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 21:25:11 +0530

    feat(dsa): Solved Preorder Traversal [Easy]

---
+++ Binary Trees/preorder traversal/solution.java
@@ -0,0 +1,10 @@
+// Problem: Preorder Traversal (Easy)
+// Topic: Binary Trees
+// Link: https://leetcode.com/problems/binary-tree-preorder-traversal/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
