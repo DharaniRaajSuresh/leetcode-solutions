0de1e7e522d6b52c1738c1f9aa35efadda957177
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:01:54 +0530

    feat(dsa): Solved Minimum number of platforms required for a railway [Medium]

---
+++ Greedy Algorithms/minimum number of platforms required for a railway/solution.java
@@ -0,0 +1,10 @@
+// Problem: Minimum number of platforms required for a railway (Medium)
+// Topic: Greedy Algorithms
+// Link: https://leetcode.com/problems/divide-intervals-into-minimum-number-of-groups/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
