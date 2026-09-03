7d40d13ceedafe710adfa4458f2c5f4424d102cd
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:02:16 +0530

    feat(dsa): Solved The N-th root of an integer [Easy]

---
+++ Binary Search/the n-th root of an integer/solution.java
@@ -0,0 +1,10 @@
+// Problem: The N-th root of an integer (Easy)
+// Topic: Binary Search
+// Link: https://www.geeksforgeeks.org/problems/find-nth-root-of-m5843/1
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
