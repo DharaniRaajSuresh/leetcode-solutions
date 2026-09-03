4cac47326c78ee36591c0c5f6794fc6885404d2f
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:00:43 +0530

    feat(dsa): Solved Merge two Sorted Lists  [Easy]

---
+++ Linked List/merge two sorted lists/solution.java
@@ -0,0 +1,10 @@
+// Problem: Merge two Sorted Lists  (Easy)
+// Topic: Linked List
+// Link: https://leetcode.com/problems/merge-two-sorted-lists/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
