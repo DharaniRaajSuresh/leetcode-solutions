f77b28ce4735c7f1709d5fca94a09cb336837d48
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:01:32 +0530

    feat(dsa): Solved 3 Sum [Medium]

---
+++ Arrays/3 sum/solution.java
@@ -0,0 +1,10 @@
+// Problem: 3 Sum (Medium)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/3sum/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
