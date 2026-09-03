b8096cc372572c962dc58a3b47d386ed25215151
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:02:20 +0530

    feat(dsa): Solved Matrix Median [Medium]

---
+++ Binary Search/matrix median/solution.java
@@ -0,0 +1,10 @@
+// Problem: Matrix Median (Medium)
+// Topic: Binary Search
+// Link: https://www.geeksforgeeks.org/problems/median-in-a-row-wise-sorted-matrix1527/1?utm_source=chatgpt.com
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
