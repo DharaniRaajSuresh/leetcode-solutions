266d80be9918478850e81f77bf0cd53b419d86b1
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 16:58:56 +0530

    feat(dsa): Solved Rotate matrix by 90 degrees [Medium]

---
+++ Arrays/rotate matrix by 90 degrees/solution.java
@@ -0,0 +1,10 @@
+// Problem: Rotate matrix by 90 degrees (Medium)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/rotate-image/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
