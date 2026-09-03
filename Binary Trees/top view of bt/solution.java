d51ffdad6279e57f73815f0706838d33ed4b20ae
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 21:27:09 +0530

    feat(dsa): Solved Top View of BT [Medium]

---
+++ Binary Trees/top view of bt/solution.java
@@ -0,0 +1,10 @@
+// Problem: Top View of BT (Medium)
+// Topic: Binary Trees
+// Link: https://www.geeksforgeeks.org/problems/top-view-of-binary-tree/1
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
