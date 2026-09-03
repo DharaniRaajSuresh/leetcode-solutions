96f18bd968aec221343b85efdd756803d27a4213
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 21:39:40 +0530

    feat(dsa): Solved Maximum Depth in BT [Easy]

---
+++ Binary Trees/maximum depth in bt/solution.java
@@ -0,0 +1,10 @@
+// Problem: Maximum Depth in BT (Easy)
+// Topic: Binary Trees
+// Link: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
