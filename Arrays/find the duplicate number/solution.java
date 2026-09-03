b563ff55cf629ed06f551b499f3ebd752b147038
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 16:59:09 +0530

    feat(dsa): Solved Find the Duplicate Number [Medium]

---
+++ Arrays/find the duplicate number/solution.java
@@ -0,0 +1,10 @@
+// Problem: Find the Duplicate Number (Medium)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/find-the-duplicate-number/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
