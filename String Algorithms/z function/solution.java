c5fe650e15a9b7a4a8d237da4df6c627ad00b345
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:03:17 +0530

    feat(dsa): Solved Z function [Easy]

---
+++ String Algorithms/z function/solution.java
@@ -0,0 +1,10 @@
+// Problem: Z function (Easy)
+// Topic: String Algorithms
+// Link: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
