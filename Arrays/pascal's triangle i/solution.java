c15c6f5d01df0e52ee2525bc5887aceac58dfc55
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 16:58:33 +0530

    feat(dsa): Solved Pascal's Triangle I [Easy]

---
+++ Arrays/pascal's triangle i/solution.java
@@ -0,0 +1,10 @@
+// Problem: Pascal's Triangle I (Easy)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/pascals-triangle/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
