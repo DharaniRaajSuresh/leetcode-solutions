e8f7a4a15c45fc2ba50fbb2ce06483cfdc8d9ab2
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:01:45 +0530

    feat(dsa): Solved Maximum Consecutive Ones [Easy]

---
+++ Arrays/maximum consecutive ones/solution.java
@@ -0,0 +1,10 @@
+// Problem: Maximum Consecutive Ones (Easy)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/max-consecutive-ones/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
