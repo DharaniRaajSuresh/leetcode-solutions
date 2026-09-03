aa8313506fe79efbc5d95c2da8273542400fb52f
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:02:51 +0530

    feat(dsa): Solved Longest Palindrome in a string [Medium]

---
+++ String Algorithms/longest palindrome in a string/solution.java
@@ -0,0 +1,10 @@
+// Problem: Longest Palindrome in a string (Medium)
+// Topic: String Algorithms
+// Link: https://leetcode.com/problems/longest-palindromic-substring/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
