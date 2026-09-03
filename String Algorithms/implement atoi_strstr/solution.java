bef86a8c33417afb35cc8f3f503b2799364c789d
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:03:00 +0530

    feat(dsa): Solved Implement ATOI/STRSTR [Medium]

---
+++ String Algorithms/implement atoi_strstr/solution.java
@@ -0,0 +1,10 @@
+// Problem: Implement ATOI/STRSTR (Medium)
+// Topic: String Algorithms
+// Link: https://leetcode.com/problems/string-to-integer-atoi/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
