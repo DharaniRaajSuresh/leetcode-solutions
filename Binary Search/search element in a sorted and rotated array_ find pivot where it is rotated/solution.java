549cf3e54d9ebe22f0149d51102149bbabdd4035
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:02:30 +0530

    feat(dsa): Solved Search element in a sorted and rotated array/ find pivot where it is rotated [Medium]

---
+++ Binary Search/search element in a sorted and rotated array_ find pivot where it is rotated/solution.java
@@ -0,0 +1,10 @@
+// Problem: Search element in a sorted and rotated array/ find pivot where it is rotated (Medium)
+// Topic: Binary Search
+// Link: https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
