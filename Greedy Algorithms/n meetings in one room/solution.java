09ac5e0c67b99ba2b950220d54705bbd21091f2a
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:01:50 +0530

    feat(dsa): Solved N meetings in one room [Easy]

---
+++ Greedy Algorithms/n meetings in one room/solution.java
@@ -0,0 +1,10 @@
+// Problem: N meetings in one room (Easy)
+// Topic: Greedy Algorithms
+// Link: https://www.geeksforgeeks.org/problems/n-meetings-in-one-room-1587115620/1
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
