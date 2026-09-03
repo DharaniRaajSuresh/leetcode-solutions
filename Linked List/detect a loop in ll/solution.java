7e98b0c6597d7e49d8962381da2697b70f028d6b
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:01:06 +0530

    feat(dsa): Solved Detect a loop in LL [Easy]

---
+++ Linked List/detect a loop in ll/solution.java
@@ -0,0 +1,10 @@
+// Problem: Detect a loop in LL (Easy)
+// Topic: Linked List
+// Link: https://leetcode.com/problems/linked-list-cycle/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
