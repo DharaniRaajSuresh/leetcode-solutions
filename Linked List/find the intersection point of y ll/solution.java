027010e6f9f1be39b6cb14e36b04b07bdafc0970
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:01:03 +0530

    feat(dsa): Solved Find the intersection point of Y LL [Easy]

---
+++ Linked List/find the intersection point of y ll/solution.java
@@ -0,0 +1,10 @@
+// Problem: Find the intersection point of Y LL (Easy)
+// Topic: Linked List
+// Link: https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
