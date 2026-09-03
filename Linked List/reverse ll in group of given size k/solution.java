b744bc275ec8a533c1c44416ecff099af3e1f3fd
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:01:10 +0530

    feat(dsa): Solved Reverse LL in group of given size K [Hard]

---
+++ Linked List/reverse ll in group of given size k/solution.java
@@ -0,0 +1,10 @@
+// Problem: Reverse LL in group of given size K (Hard)
+// Topic: Linked List
+// Link: https://leetcode.com/problems/reverse-nodes-in-k-group/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
