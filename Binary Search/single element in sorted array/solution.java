c1fd13958a3e202aa99957561faf3d75481aa5cf
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:02:25 +0530

    feat(dsa): Solved Single element in sorted array [Medium]

---
+++ Binary Search/single element in sorted array/solution.java
@@ -0,0 +1,10 @@
+// Problem: Single element in sorted array (Medium)
+// Topic: Binary Search
+// Link: https://leetcode.com/problems/single-element-in-a-sorted-array/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
