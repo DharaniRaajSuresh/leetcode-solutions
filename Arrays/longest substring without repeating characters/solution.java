b25d115710e2954026d8b6aac3b9da2164f31b0d
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:00:27 +0530

    feat(dsa): Solved Longest Substring Without Repeating Characters [Medium]

---
+++ Arrays/longest substring without repeating characters/solution.java
@@ -0,0 +1,10 @@
+// Problem: Longest Substring Without Repeating Characters (Medium)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
