112e562b75b3c39975222a4ca8f94fe19a5b11c2
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 16:59:24 +0530

    feat(dsa): Solved Search in a 2D matrix [Medium]

---
+++ Arrays/search in a 2d matrix/solution.java
@@ -0,0 +1,10 @@
+// Problem: Search in a 2D matrix (Medium)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/search-a-2d-matrix/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
