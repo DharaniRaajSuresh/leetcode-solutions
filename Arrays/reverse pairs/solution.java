837b0b3c4f3584c243a1575e4e07a6b610240304
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 16:59:51 +0530

    feat(dsa): Solved Reverse Pairs [Hard]

---
+++ Arrays/reverse pairs/solution.java
@@ -0,0 +1,10 @@
+// Problem: Reverse Pairs (Hard)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/reverse-pairs/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
