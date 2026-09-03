bf76c2fadba9d5b37e6a8411d81ff7e09af37684
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 16:59:29 +0530

    feat(dsa): Solved Pow(x, n) [Medium]

---
+++ Arrays/pow(x, n)/solution.java
@@ -0,0 +1,10 @@
+// Problem: Pow(x, n) (Medium)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/powx-n/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
