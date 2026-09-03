0d95e5a293912bf99e5d74c387a15a9011e82559
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 16:59:00 +0530

    feat(dsa): Solved Merge Overlapping Subintervals [Medium]

---
+++ Arrays/merge overlapping subintervals/solution.java
@@ -0,0 +1,10 @@
+// Problem: Merge Overlapping Subintervals (Medium)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/merge-intervals/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
