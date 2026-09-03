916b40b59e3a0e75688b99355bed83a5720942c0
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 16:59:33 +0530

    feat(dsa): Solved Majority Element-I [Easy]

---
+++ Arrays/majority element-i/solution.java
@@ -0,0 +1,10 @@
+// Problem: Majority Element-I (Easy)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/majority-element/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
