996db135e8098fd7027d67d972d75803558e3216
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 16:59:20 +0530

    feat(dsa): Solved Inversion of Array (Pre-req: Merge Sort) [Medium]

---
+++ Arrays/inversion of array (pre-req_ merge sort)/solution.java
@@ -0,0 +1,10 @@
+// Problem: Inversion of Array (Pre-req: Merge Sort) (Medium)
+// Topic: Arrays
+// Link: https://www.geeksforgeeks.org/problems/inversion-of-array-1587115620/1
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
