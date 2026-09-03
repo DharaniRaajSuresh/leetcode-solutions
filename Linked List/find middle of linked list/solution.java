eb8b00eaf8f5f5c3f0b644934c4740f31230e447
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:00:37 +0530

    feat(dsa): Solved Find Middle of Linked List [Easy]

---
+++ Linked List/find middle of linked list/solution.java
@@ -0,0 +1,10 @@
+// Problem: Find Middle of Linked List (Easy)
+// Topic: Linked List
+// Link: https://leetcode.com/problems/middle-of-the-linked-list/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
