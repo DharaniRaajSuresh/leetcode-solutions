547de6af377ac294179154412c325b2645d6c607
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 16:58:37 +0530

    feat(dsa): Solved Next Permutation [Medium]

---
+++ Arrays/next permutation/solution.java
@@ -0,0 +1,10 @@
+// Problem: Next Permutation (Medium)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/next-permutation/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
