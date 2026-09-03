878ee18eb2a6a86104f4d1a785a4cc1bfc5660b9
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 16:59:37 +0530

    feat(dsa): Solved Majority Element-II [Medium]

---
+++ Arrays/majority element-ii/solution.java
@@ -0,0 +1,10 @@
+// Problem: Majority Element-II (Medium)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/majority-element-ii/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
