591e7a2ede1e0d4c653e184d2f319bc1574f2522
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 16:59:56 +0530

    feat(dsa): Solved Two Sum [Easy]

---
+++ Arrays/two sum/solution.java
@@ -0,0 +1,10 @@
+// Problem: Two Sum (Easy)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/two-sum/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
