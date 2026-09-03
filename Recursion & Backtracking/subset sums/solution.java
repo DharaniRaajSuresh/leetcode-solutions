76cbc1aa523797c108147662c9b49857b29aafd7
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:02:03 +0530

    feat(dsa): Solved Subset Sums [Medium]

---
+++ Recursion & Backtracking/subset sums/solution.java
@@ -0,0 +1,10 @@
+// Problem: Subset Sums (Medium)
+// Topic: Recursion & Backtracking
+// Link: https://www.geeksforgeeks.org/problems/subset-sums2234/1
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
