ca1c4b14e93a40d51d73d73db42dbcf6bf8a6ce8
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:01:58 +0530

    feat(dsa): Solved Job sequencing Problem [Medium]

---
+++ Greedy Algorithms/job sequencing problem/solution.java
@@ -0,0 +1,10 @@
+// Problem: Job sequencing Problem (Medium)
+// Topic: Greedy Algorithms
+// Link: https://www.geeksforgeeks.org/problems/job-sequencing-problem-1587115620/1
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
