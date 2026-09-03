e357da6efd8cddb02b5d38d8ded7138c7223ae66
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 21:25:47 +0530

    feat(dsa): Solved Inorder Traversal [Easy]

---
+++ Binary Trees/inorder traversal/solution.java
@@ -0,0 +1,10 @@
+// Problem: Inorder Traversal (Easy)
+// Topic: Binary Trees
+// Link: https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
