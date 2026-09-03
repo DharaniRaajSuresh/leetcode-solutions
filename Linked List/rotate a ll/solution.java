32c654a8afd10f708b4ec6356226d0d842c09dba
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:01:28 +0530

    feat(dsa): Solved Rotate a LL [Medium]

---
+++ Linked List/rotate a ll/solution.java
@@ -0,0 +1,10 @@
+// Problem: Rotate a LL (Medium)
+// Topic: Linked List
+// Link: https://leetcode.com/problems/rotate-list/description/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
