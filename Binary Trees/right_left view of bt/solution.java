a7d4578308508e436714c6b12d85033dba661925
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 21:26:07 +0530

    feat(dsa): Solved Right/Left View of BT [Medium]

---
+++ Binary Trees/right_left view of bt/solution.java
@@ -0,0 +1,10 @@
+// Problem: Right/Left View of BT (Medium)
+// Topic: Binary Trees
+// Link: https://leetcode.com/problems/binary-tree-right-side-view/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
