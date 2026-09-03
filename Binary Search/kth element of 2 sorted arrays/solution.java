027083f04ddea6fdab1d12022c3c30e5c8c47225
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:02:38 +0530

    feat(dsa): Solved Kth element of 2 sorted arrays [Medium]

---
+++ Binary Search/kth element of 2 sorted arrays/solution.java
@@ -0,0 +1,10 @@
+// Problem: Kth element of 2 sorted arrays (Medium)
+// Topic: Binary Search
+// Link: https://www.geeksforgeeks.org/problems/k-th-element-of-two-sorted-array1317/1
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
