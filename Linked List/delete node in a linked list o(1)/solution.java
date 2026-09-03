52aab1143db9bf7144a7e2a968e87472285baa70
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:00:58 +0530

    feat(dsa): Solved Delete Node in a Linked List O(1) [Medium]

---
+++ Linked List/delete node in a linked list o(1)/solution.java
@@ -0,0 +1,10 @@
+// Problem: Delete Node in a Linked List O(1) (Medium)
+// Topic: Linked List
+// Link: https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
