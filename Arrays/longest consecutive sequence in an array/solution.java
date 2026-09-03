432c76718b1dc987ab7304393a94e1c19d51817c
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:00:07 +0530

    feat(dsa): Solved Longest Consecutive Sequence in an Array [Medium]

---
+++ Arrays/longest consecutive sequence in an array/solution.java
@@ -0,0 +1,10 @@
+// Problem: Longest Consecutive Sequence in an Array (Medium)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/longest-consecutive-sequence/solution/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
