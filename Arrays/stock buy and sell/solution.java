656db06061cd73ef843c3fa78a84e65c74117e48
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 16:58:52 +0530

    feat(dsa): Solved Stock Buy and Sell [Easy]

---
+++ Arrays/stock buy and sell/solution.java
@@ -0,0 +1,10 @@
+// Problem: Stock Buy and Sell (Easy)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
