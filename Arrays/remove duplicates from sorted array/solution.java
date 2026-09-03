7f8a56a4dbdfdae1afc9c4d4acba93e0fb794947
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:01:41 +0530

    feat(dsa): Solved Remove duplicates from sorted array [Easy]

---
+++ Arrays/remove duplicates from sorted array/solution.java
@@ -0,0 +1,10 @@
+// Problem: Remove duplicates from sorted array (Easy)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/#:~:text=Input%3A%20nums%20%3D%20%5B0%2C,%2C%203%2C%20and%204%20respectively.
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
