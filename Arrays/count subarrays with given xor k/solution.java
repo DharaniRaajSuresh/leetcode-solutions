b25c53441830aea16014abdf6759301d71262c96
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:00:16 +0530

    feat(dsa): Solved Count subarrays with given xor K [Medium]

---
+++ Arrays/count subarrays with given xor k/solution.java
@@ -0,0 +1,10 @@
+// Problem: Count subarrays with given xor K (Medium)
+// Topic: Arrays
+// Link: https://www.naukri.com/code360/problems/subarrays-with-xor-k_6826258
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
