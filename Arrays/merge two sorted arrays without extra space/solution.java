7f8379d280efaee4eff5a220442b241c96ff0f9a
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 16:59:05 +0530

    feat(dsa): Solved Merge two sorted arrays without extra space [Easy]

---
+++ Arrays/merge two sorted arrays without extra space/solution.java
@@ -0,0 +1,10 @@
+// Problem: Merge two sorted arrays without extra space (Easy)
+// Topic: Arrays
+// Link: https://leetcode.com/problems/merge-sorted-array/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
