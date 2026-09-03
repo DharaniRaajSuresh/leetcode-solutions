9e241550482d34a5e6ba7be47c7c7cac82516012
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:00:54 +0530

    feat(dsa): Solved Add two numbers as LinkedList [Medium]

---
+++ Linked List/add two numbers as linkedlist/solution.java
@@ -0,0 +1,10 @@
+// Problem: Add two numbers as LinkedList (Medium)
+// Topic: Linked List
+// Link: https://leetcode.com/problems/add-two-numbers/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
