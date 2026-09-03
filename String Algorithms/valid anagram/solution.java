1afbd0770d46fbc352364ed324307d3d048b662d
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-03 17:03:22 +0530

    feat(dsa): Solved Valid Anagram [Easy]

---
+++ String Algorithms/valid anagram/solution.java
@@ -0,0 +1,10 @@
+// Problem: Valid Anagram (Easy)
+// Topic: String Algorithms
+// Link: https://leetcode.com/problems/valid-anagram/#:~:text=Given%20two%20strings%20s%20and,the%20original%20letters%20exactly%20once.\u0026text=Constraints%3A,.length%20%3C%3D%205%20*%2010
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
