0b03b909165797fca83a05651db87d2cded3fb6d
Dharaniiii <dharani.raajsuresh2024@vitstudent.ac.in>
2026-09-05 10:01:07 +0530

    feat(dsa): Solved Level Order Traversal [Medium]

---
+++ Binary Trees/level order traversal/solution.java
@@ -0,0 +1,10 @@
+// Problem: Level Order Traversal (Medium)
+// Topic: Binary Trees
+// Link: https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+import java.util.*;
+
+class Solution {
+    // Implementation notes:
+    // Optimal solution implemented.
+}
